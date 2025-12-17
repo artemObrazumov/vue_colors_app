a2b3ffbc45c1a42340ba866bc9f3fc5f6f0ad80d
artemobraz <artemobraz@yandex-team.ru>
2025-12-17 18:21:43 +0300

    feat: add github pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue_colors_app/',
   plugins: [
     vue(),
     vueDevTools(),
